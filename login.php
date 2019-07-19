9df7dcd279d7b0ef7bd704deb9bbe1a593f1093c
Iqra-Nazeer <52270717+Iqra-Nazeer@users.noreply.github.com>
2019-07-19 19:40:06 +0500

    first commit
    nothing

--- login.php
+++ login.php
@@ -1,29 +1,30 @@
 <?php require "config.php"; ?> 
 <?php
 if(isset($_POST["login"])){
- if(!empty($_POST['firstname']) && !empty($_POST['password'])){
- $firstname = $_POST['firstname'];
- $password = $_POST['password'];
- $query = mysqli_query($conn, "SELECT * FROM tb_employees WHERE firstname='".$firstname."' AND password='".$password."'");
+ if(!empty($_POST['email']) && !empty($_POST['password'])){
+ $email = $_POST['email'];
+ $password = md5($_POST['password']); //md5 used to encrypt database password.
+ $query = mysqli_query($conn, "SELECT * FROM tb_employees WHERE email='".$email."' AND password='".$password."'");
  $numrows = mysqli_num_rows($query);
  if($numrows !=0)
  {
+     //mysqli_fetch_assoc() function fetches a result row as an associative array.
  while($row = mysqli_fetch_assoc($query)){
  
- $dbfirstname=$row['firstname'];
+ $dbemail=$row['email'];
  $dbpassword=$row['password'];
  }
- if($firstname == $dbfirstname && $password == $dbpassword)
+ if($email == $dbemail && $password == $dbpassword)
  {
  session_start();
- $_SESSION['sess_user']=$firstname;
- //Redirect Browser
+ $_SESSION['sess_user']=$email;
+ //Redirect Employees Data
  header("Location:index.php");
  }
  }
  else
  {
- echo "Invalid Username or Password!";
+ echo "Invalid Email or Password!";
  }
  }
  else
@@ -47,11 +48,11 @@ if(isset($_POST["login"])){
         <div class="col-lg-3">
             <form action="login.php" method="POST">
                 <div class="form-group">
-                    <label for="firstname">Firstname</label>
-                    <input name="firstname"  id="firstname" type="text" class="form-control" placeholder="firstname" required>
+                    <label for="email">Email</label> <!-- iqranazir@gmail.com , sidranazir@gmail.com, faryalnazir@gmail.com, tomjerry@gmail.com -->
+                    <input name="email"  id="email" type="text" class="form-control" placeholder="email" required>
                 </div>
                 <div class="form-group">
-                    <label for="password">Password</label>
+                    <label for="password">Password</label><!-- iqra123, sidra123, faryal123, tom123 -->
                     <input name="password" id="password" type="password" class="form-control" placeholder="password" required>
                 </div>
                 <div class="form-group">
